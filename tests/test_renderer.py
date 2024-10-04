bcd8b1513299b4503ec89201628e24244dbfa2da
Kyle Diaz <dev@kylediaz.com>
2024-10-04 16:24:44 -0400

    Change tests to use unittest assert

--- tests/test_renderer.py
+++ tests/test_renderer.py
@@ -1,26 +1,26 @@
 import unittest
 from gator.engine import Environment, Template, CONTENT
 
-class TestRenderer(unittest.TestCase):
+class Testrender_to_strer(unittest.TestCase):
 
-    def test_template_render(self):
+    def test_template_render_to_str(self):
         env = Environment()
 
         env.template["t"] = Template.from_str("[t content]")
-        assert Template.from_str("<template t='t'></template>").render(env) == "[t content]"
+        self.assertEqual(Template.from_str("<template t='t'></template>").render_to_str(env), "[t content]")
 
         env.var[CONTENT] = "[content]"
-        assert Template.from_str("[t]<content />[t]").render(env) == '[t][content][t]'
+        self.assertEqual(Template.from_str("[t]<content />[t]").render_to_str(env), '[t][content][t]')
         env.var[CONTENT] = None
 
         env.template["tc"] = Template.from_str("[t]<content/>[t]")
-        assert Template.from_str("[T]<template t='tc'>[content]</template>[T]").render(env) == "[T][t][content][t][T]"
+        self.assertEqual(Template.from_str("[T]<template t='tc'>[content]</template>[T]").render_to_str(env), "[T][t][content][t][T]")
 
     def test_template_vars(self):
         env = Environment()
 
         env.template["t"] = Template.from_str("[key={{ $value }}]")
-        assert Template.from_str("[t]<template t='t', value='value'></template>[t]").render(env) == "[t][key=value][t]"
+        self.assertEqual(Template.from_str("[t]<template t='t', value='value'></template>[t]").render_to_str(env), "[t][key=value][t]")
 
 if __name__ == '__main__':
     unittest.main()
